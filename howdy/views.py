6a2d2ce77acf66a673a8382ad0ac4bcaf0c9c5ae
vuonglm <vuonglm@nal.vn>
2018-07-10 10:34:00 +0700

    first commit

---
+++ howdy/views.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+# Create your views here.
+class HomePageView(TemplateView):
+	def get(self, request, **kwargs):
+		return render(request, 'index.html', context=None)
+		
+class AboutPageView(TemplateView):
+	template_name = 'about.html'
